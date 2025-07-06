84c62cd1bb062bed2ea821f4e861ec8142f19353
Karanam Mohan Rahul <rahul02.bunny@gmail.com>
2025-07-06 18:16:25 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -47,7 +47,7 @@ app.use(helmet({
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','https://gurukul-project.vercel.app'],
+  origin: ['http://localhost:5173', 'http://localhost:5174','https://gurukul-project.vercel.app','https://gurukul-project-ansl.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
